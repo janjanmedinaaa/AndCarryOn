c74407ec0703820264f7a6adc87b341d04206fd6
Janjan Medina <jmedina@isbx.com>
2020-05-17 15:43:38 +0800

    Initial Commit w/ Chrome Extension

---
+++ chrome-extension/scripts/background.js
@@ -0,0 +1,41 @@
+const JSONBOX_ID = 'box_daa5a833190893fb9a3b'
+const LAST_REQUEST_ID = 'LAST_REQUEST_ID'
+const REQUEST_ALARM = 'REQUEST_ALARM'
+const BASE_URL = `https://jsonbox.io/${JSONBOX_ID}?limit=1`
+
+function openTab(url) {
+  chrome.tabs.create({ url })
+}
+
+function saveLastRequestID(id) {
+  let options = {}
+  options[LAST_REQUEST_ID] = id
+
+  chrome.storage.sync.set(options)
+}
+
+function validateLastRequestID(response) {
+  if (response.length == 0) return
+
+  var id = response[0]._id
+  chrome.storage.sync.get([LAST_REQUEST_ID], function(result) {
+    if (result[LAST_REQUEST_ID] == id) return
+    saveLastRequestID(id)
+    openTab(response[0].url)
+  });
+}
+
+function requestLatestURL() {
+  fetch(BASE_URL)
+    .then(response => response.json())
+    .then(data => validateLastRequestID(data))
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.alarms.create(REQUEST_ALARM, { periodInMinutes: 0.1 });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name != REQUEST_ALARM) return
+  requestLatestURL()
+});
---
+++ js/index.js
@@ -0,0 +1,46 @@
+(function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+     .register('/AndCarryOn/sw.js', { scope: '/AndCarryOn/' })
+     .then(function() { 
+        console.log('Service Worker Registered'); 
+      });
+  }
+  toggleSpinner(false)
+})()
+
+const JSONBOX_ID = 'box_daa5a833190893fb9a3b'
+const BASE_URL = `https://jsonbox.io/${JSONBOX_ID}`
+
+function sendRequest(url) {
+  toggleSpinner(true)
+  disableButton(true)
+
+  fetch(BASE_URL, {
+    method: 'post',
+    body: JSON.stringify({ url })
+  })
+  .then(response => response.json())
+  .then(() => {
+    document.getElementById('carryOnUrl').value = ''
+    toggleSpinner(false)
+    disableButton(false)
+  })
+  .catch(() => {
+    toggleSpinner(false)
+    disableButton(false)
+  })
+}
+
+function toggleSpinner(show) {
+  document.getElementById('carryOnSpinner').hidden = !show
+}
+
+function disableButton(disable) {
+  document.getElementById('carryOnButton').disabled = disable
+}
+
+function carryOnClicked() {
+  var url = document.getElementById('carryOnUrl').value
+  sendRequest(url)
+}
